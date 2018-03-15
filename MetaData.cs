aeb15f7418889f5d23738ebfe15a4dd205197e42
Howard N Davis <hndavis983@outlook.com>
2018-03-15 00:22:04 -0400

    new file:   .gitignore 	new file:   App.config 	new file:   Form1.Designer.cs 	new file:   Form1.cs 	new file:   MetaData.cs 	new file:   MetaDataEditor.csproj 	new file:   MetaRowValues.cs 	new file:   Program.cs 	new file:   Properties/AssemblyInfo.cs 	new file:   Properties/Resources.Designer.cs 	new file:   Properties/Resources.resx 	new file:   Properties/Settings.Designer.cs 	new file:   Properties/Settings.settings

---
+++ MetaData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MetaDataEditor
+{
+	public class MetaData
+	{
+		List<string> Names = new List<string>();
+		List<List<string>> vals = new List<List<string>>();
+		String Path{ get; set; }
+
+		MetaData(string path)
+		{
+			Path = path;
+		}
+		public bool Load(string path)
+		{
+			XmlDocument doc = new XmlDocument();
+			doc.Load(path);
+			string xmlcontents = doc.InnerXml;
+			return false;
+		}
+
+
+		public bool SaveAs()
+		{
+			return Save();
+		}
+		public bool Save()
+		{
+			return false;
+		}
+	}
+}
---
+++ MetaRowValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaDataEditor
+{
+	public class MetaRowValue
+	{
+		List<string> values = new List<string>(30);
+	}
+}
